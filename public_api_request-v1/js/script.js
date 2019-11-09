5d0dcc6cbd7d34912de198a99b2ad48883b8a6e6
Rohan Gupta <53973276+rohanxyzg@users.noreply.github.com>
2019-11-09 20:48:41 +0530

    Updated script.js

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -1,33 +1,34 @@
-const galleryDiv = document.getElementById('gallery');
+const usersURL = 'https://randomuser.me/api/';
+const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
-function fetchData(url){
-    return fetch(url)
-        .then(res=>res.json())
-        .catch(error => console.log('Problem',error));
+const modalDiv = document.getElementById('modal-div');
+async function fetchData(url){
+    try{
+        const response = await fetch(url);
+        return await response.json();
+    }catch(error){
+        throw error;
+    }
 }
-
-async function getRandomUsers(numOfUsers){
-    const users = [];
-    for(let i=0;i<numOfUsers;i++){
-        await fetchData('https://randomuser.me/api/')
-            .then(data=>users.push(data.results[0]));
+//fetchData(usersURL);
+const users = [];
+async function getRandomUsers(num){
+    
+    for(let i=0;i<num;i++){
+        const currentUser = await fetchData(usersURL);
+       // console.log(currentUser.results[0]);
+        users.push(currentUser.results[0]);
     }
     return users;
 }
+//console.log(getRandomUsers(12));
 
-
-
-function generateGalleryMarkup(users){
-    console.log(users);
-    console.log(typeof users);
-    console.log(users.values())
-    users.map(user=>{
-        //const card = document.createElement('div');
+function generateGalleryHTML(data){
+    data.map(user=>{
         const name = user.name.first+' '+user.name.last;
         const img = user.picture.large;
         const email = user.email;
         const city = user.location.city;
-        console.log("Hi");
         const html = `
         <div class="card">
             <div class="card-img-container">
@@ -38,78 +39,70 @@ function generateGalleryMarkup(users){
                 <p class="card-text">${email}</p>
                 <p class="card-text cap">${city}</p>
             </div>
-        </div>
-        `;
-        //card.className = 'card';
-        galleryDiv.innerHTML += html;
-        //galleryDiv.append(card);
+        </div>`;
+        gallery.innerHTML += html;
     });
-    console.log(users);
     return users;
+   
 }
+function generateModalHTML(users){
 
-function generateModalMarkup(data,users){
-    const modalContainer = document.createElement('div');
-    const name = data.name.first+' '+data.name.last;
-    const img = data.picture.large;
-    const email = data.email;
-    const city = data.location.city;
-    const cell = data.cell;
-    const address = data.location.street.name+' '+city+' '+data.location.postcode;
-    const birthday = data.dob.date;
-    const html = `
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src='${img}' alt="profile picture">
-            <h3 id="name" class="modal-name cap">${name}</h3>
-            <p class="modal-text">${email}</p>
-            <p class="modal-text cap">${city}</p>
-            <hr>
-            <p class="modal-text">${cell}</p>
-            <p class="modal-text">${address}</p>
-            <p class="modal-text">${birthday}</p>
+    let modalHTML = '';
+    
+    users.forEach((user, index) => {
+        // references for user information
+        let userName = user.name;
+        let userMail = user.email;
+        let userImg = user.picture;
+        let userLocation = user.location;
+        let userDob = user.dob;
+        let userPhone = user.phone;
+        modalHTML += `
+        <div class = "modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${userImg.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${userName.first} ${userName.last}</h3>
+                    <p class="modal-text">${userMail}l</p>
+                    <p class="modal-text cap">${userLocation.city}</p>
+                    <hr>
+                    <p class="modal-text">${userPhone}</p>
+                    <p class="modal-text">${userLocation.street}, ${userLocation.city}, ${userLocation.state} ${userLocation.postcode}</p>
+                    <p class="modal-text">Birthday: ${userDob.date.substring(0, 10)}</p>
+                </div>
+            </div>
         </div>
-    </div>
+        
+        `; 
 
-    // IMPORTANT: Below is only for exceeds tasks 
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>
-    `;
-    modalContainer.className = 'modal-container';
-    modalContainer.innerHTML += html;
-    body.appendChild(modalContainer);
-    generateModalListeners(modalContainer,users);
+    }); 
+    
+    modalDiv.innerHTML = modalHTML;
 
-}
-function generateModalListeners(modalContainer,users){
-    const exitButton = modalContainer.querySelector('#modal-close-btn');
-    exitButton.addEventListener('click',()=>modalContainer.remove());
-}
-function generateCardsListeners(users){
-    const cards = galleryDiv.children;
-    console.log(cards);
-    for(let card of cards){
-        card.addEventListener('click',()=>{
-            const email = card.querySelector('p').textContent;
-            console.log(email);
-            for(let user of users){
-                if(user.email === email){
-                    data=user;
-                    break;
-                }
-            }
-            generateModalMarkup(data,users);
-        })
-    }
+    $('.modal-container').hide();
 }
 
+function displayModalElements(){
+    $('.card').click((e)=>{
+        const parentCard = $(e.target).parentsUntil('.gallery');
+        const $name = $(parentCard).find('.card-info-container>h3').text();
+        console.log($name);
+        const $currentModal = $(`.modal-container:has(h3:contains(${$name}))`);
+        $currentModal.show();
+    })
+}
+function closeButton(){
+    $('.modal-close-btn').click((e)=>{
+        let parentModalContainer= $(e.target).parents('.modal-container')[0];
+        $(parentModalContainer).hide();
+    })
+}
+getRandomUsers(12)
+    .then(generateGalleryHTML)
+    .then(generateModalHTML)
+    .then(displayModalElements)
+    .then(closeButton);
+// generateGalleryHTML(getRandomUsers(12));
 
-const promiseObject = getRandomUsers(12);
-console.log(promiseObject);
-promiseObject
-    .then(generateGalleryMarkup)
-    .then(generateCardsListeners)
-    .catch(err => console.log('Oops... An error occured:', err));
+
